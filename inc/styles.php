34939ac349cb28c87a6d12f09e424e04376b9e8c
Chris G. Clapp <chris@chisosdesigns.com>
2015-01-28 10:46:32 -0700

    Update Font Awesome up to 4.3.0

--- inc/styles.php
+++ inc/styles.php
@@ -6,7 +6,7 @@ function agi_styles() {
 	wp_register_style( 'main_css', get_stylesheet_directory_uri() . '/css/style.css', false, '1.0', 'all' );
 	wp_enqueue_style( 'main_css' );
 
-	wp_register_style( 'font_awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css', false, '4.2', 'all' );
+	wp_register_style( 'font_awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css', false, '4.3', 'all' );
 	wp_enqueue_style( 'font_awesome' );
 
 }
