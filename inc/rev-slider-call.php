f959dd0c4857712122b4734de6df5179d08d3db0
Chris G. Clapp <chris@chisosdesigns.com>
2015-02-26 16:43:22 -0700

    Add in slider function

---
+++ inc/rev-slider-call.php
@@ -0,0 +1,21 @@
+<?php
+	
+	function put_rev_slider($echo = TRUE) {
+		$slider_slug = get_option('home_page_slider');
+		
+		if(!empty($slider_slug) && shortcode_exists('rev_slider')) {
+			$to_return = do_shortcode("[rev_slider $slider_slug]");
+		} else {
+			$to_return = "
+			<div id=\"no-ss\">
+				<img src=\"http://placehold.it/1040x385/0640aa/FFFFFF/&text=Slider+to+go+here\" />
+			</div>
+			";
+		}
+		if($echo) {
+			echo $to_return;
+			return TRUE;
+		} else {
+			return $to_return;
+		}
+	}
