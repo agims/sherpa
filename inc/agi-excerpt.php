03c735d52df3530c2032693784e48a85579a19a5
Chris G. Clapp <chris@chisosdesigns.com>
2015-02-26 14:51:25 -0700

    Fix agi-excerpt

--- inc/agi-excerpt.php
+++ inc/agi-excerpt.php
@@ -4,7 +4,7 @@ function agi_excerpt() {
 	$excerpt = get_the_excerpt();
 	$permalink = get_permalink();
 	echo $excerpt;
-	echo '<a href="' . $permalink . '" class="btn btn-read-more pull-right">' . _e('Read More', 'sherpa') . '</a>' . "\n";
+	echo '<a href="' . $permalink . '" class="btn btn-read-more pull-right">' . __('Read More', 'sherpa') . '</a>' . "\n";
 	echo '<div class="clear"></div>';
 	
 }
