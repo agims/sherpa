50cf371f1ad109d90b3698d6bed7a0b3d91e1c95
Chris G. Clapp <chris@chisosdesigns.com>
2015-03-17 17:04:47 -0600

    Update modernizr Update scripts to load typekit into backend

--- inc/scripts.php
+++ inc/scripts.php
@@ -1,26 +1,37 @@
 <?php
 
-// Register Script
-function agi_scripts() {
+function agi_front_and_backend_scripts() {
+	
+	// DON'T FORGET TO CHANGE WHICH KIT!!!
+	wp_register_script( 'typekit', '//use.typekit.net/vfh6shm.js', false, false, false );
+	wp_enqueue_script( 'typekit' );
+	
+	wp_register_script( 'typekit_init', get_template_directory_uri() . '/js/typekit-init.js', array('typekit'), '1', false);
+	wp_enqueue_script( 'typekit_init' );	
+}
 
-	wp_register_script( 'modernizr_respond', get_stylesheet_directory_uri() . '/js/vendor/modernizr-2.6.2-respond-1.1.0.min.js', false, '2.6.2', false );
+function agi_frontend_scripts() {
+	wp_register_script( 'modernizr_respond', get_stylesheet_directory_uri() . '/js/vendor/modernizr-2.8.3-respond-1.4.2.min.js', false, '2.8.3', false );
 	wp_enqueue_script( 'modernizr_respond' );
 
 	wp_deregister_script( 'jquery' );
 	wp_register_script( 'jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js', false, '1.11.2', false );
 	wp_enqueue_script( 'jquery' );
-	
-	wp_register_script( 'typekit', '//use.typekit.net/vfh6shm.js', false, false, false );
-	wp_enqueue_script( 'typekit' );
-	
-	wp_register_script( 'typekit_init', get_template_directory_uri() . '/js/typekit-init.js', array('typekit'), '1', false);
-	wp_enqueue_script( 'typekit_init' );
 
 	wp_register_script( 'main_js', get_stylesheet_directory_uri() . '/js/main-ck.js', array( 'jquery'), '1.0.0', true );
 	wp_enqueue_script( 'main_js' );
 
+	agi_front_and_backend_scripts();	
 }
 
-// Hook into the 'wp_enqueue_scripts' action
-add_action( 'wp_enqueue_scripts', 'agi_scripts' );
+function agi_backend_scripts() {
+	
+	
+	agi_front_and_backend_scripts();
+}
+
+
+add_action( 'wp_enqueue_scripts', 'agi_frontend_scripts' );
+
+add_action('admin_enqueue_scripts', 'agi_backend_scripts');
 
