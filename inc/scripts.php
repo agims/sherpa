deb3bb2fea42726c3f924892c7405639484f61fb
Chris G. Clapp <chris@chrisclapp.com>
2016-02-21 10:31:56 -0700

    Changing AGI references to Sherpa

--- inc/scripts.php
+++ inc/scripts.php
@@ -1,6 +1,6 @@
 <?php
 
-function agi_front_and_backend_scripts() {
+function sherpa_front_and_backend_scripts() {
 
 	$typekit = get_option('typekit');
 	
@@ -13,7 +13,7 @@ function agi_front_and_backend_scripts() {
 	}
 }
 
-function agi_frontend_scripts() {
+function sherpa_frontend_scripts() {
 	wp_register_script( 'modernizr_respond', get_stylesheet_directory_uri() . '/js/vendor/modernizr-2.8.3-respond-1.4.2.min.js', false, '2.8.3', false );
 	wp_enqueue_script( 'modernizr_respond' );
 
@@ -21,20 +21,24 @@ function agi_frontend_scripts() {
 	wp_register_script( 'jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js', false, '1.11.2', false );
 	wp_enqueue_script( 'jquery' );
 
-	wp_register_script( 'main_js', get_stylesheet_directory_uri() . '/js/main-ck.js', array( 'jquery'), '1.0.0', true );
+	if(file_exists(__DIR__ . '/../js/main-ck.js')) {
+		wp_register_script( 'main_js', get_stylesheet_directory_uri() . '/js/main-ck.js', array( 'jquery'), '1.0.0', true );	
+	} else {
+		wp_register_script( 'main_js', get_stylesheet_directory_uri() . '/js/main.js', array( 'jquery'), '1.0.0', true );
+	}
 	wp_enqueue_script( 'main_js' );
 
-	agi_front_and_backend_scripts();	
+	sherpa_front_and_backend_scripts();	
 }
 
-function agi_backend_scripts() {
+function sherpa_backend_scripts() {
 	
 	
-	agi_front_and_backend_scripts();
+	sherpa_front_and_backend_scripts();
 }
 
 
-add_action( 'wp_enqueue_scripts', 'agi_frontend_scripts' );
+add_action( 'wp_enqueue_scripts', 'sherpa_frontend_scripts' );
 
-add_action('admin_enqueue_scripts', 'agi_backend_scripts');
+add_action('admin_enqueue_scripts', 'sherpa_backend_scripts');
 
--- inc/styles.php
+++ inc/styles.php
@@ -1,9 +1,16 @@
 <?php
 
 // Register Style
-function agi_styles() {
+function sherpa_styles() {
 
-	wp_register_style( 'main_css', get_stylesheet_directory_uri() . '/css/style.css', false, '1.0', 'all' );
+	// We are going to defer loading our styles until after WooCommerce has loaded its styles IF WooCommerce exists
+	if(function_exists('is_woocommerce')) {
+		$main_css_deps = array('woocommerce-layout');
+	} else {
+		$main_css_deps = FALSE;
+	}
+	
+	wp_register_style( 'main_css', get_stylesheet_directory_uri() . '/css/style.css', $main_css_deps, '1.0', 'all' );
 	wp_enqueue_style( 'main_css' );
 
 	wp_register_style( 'font_awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css', false, '4.3', 'all' );
@@ -12,6 +19,6 @@ function agi_styles() {
 }
 
 // Hook into the 'wp_enqueue_scripts' action
-add_action( 'wp_enqueue_scripts', 'agi_styles' );
+add_action( 'wp_enqueue_scripts', 'sherpa_styles' );
 	
 ?>
