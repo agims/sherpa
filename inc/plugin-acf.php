e69c1cbb27579b7dcd8cef09c157684c95964ba4
Chris Clapp <chris@chrisclapp.com>
2017-08-08 12:06:33 -0600

    Version 3.6.3 - Nuptse * Include Advanced Custom Fields within the theme in /plugins/ * Remove ACF from recommended plugins

---
+++ inc/plugin-acf.php
@@ -0,0 +1,36 @@
+<?php 
+
+// 1. customize ACF path
+add_filter('acf/settings/path', 'my_acf_settings_path');
+ 
+function my_acf_settings_path( $path ) {
+ 
+    // update path
+    $path = get_stylesheet_directory() . '/plugins/acf/';
+    
+    // return
+    return $path;
+    
+}
+ 
+
+// 2. customize ACF dir
+add_filter('acf/settings/dir', 'my_acf_settings_dir');
+ 
+function my_acf_settings_dir( $dir ) {
+ 
+    // update path
+    $dir = get_stylesheet_directory_uri() . '/plugins/acf/';
+    
+    // return
+    return $dir;
+    
+}
+ 
+
+// 3. Hide ACF field group menu item
+add_filter('acf/settings/show_admin', '__return_false');
+
+
+// 4. Include ACF
+include_once( get_stylesheet_directory() . '/plugins/acf/acf.php' );
